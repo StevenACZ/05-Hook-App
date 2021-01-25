b245c14cf97dac35ae3237ad628b2693b2ff31b7
stevenacz <scoailazaa@gmail.com>
2021-01-25 16:36:25 -0500

    useEffect - precauciones

--- src/components/02-useEffect/Message.js
+++ src/components/02-useEffect/Message.js
@@ -1,10 +1,17 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 
 const Message = () => {
+  const [coords, setCoords] = useState({ x: 0, y: 0 })
+  const { x, y } = coords;
+
   useEffect(() => {
-    console.log('componente montado');
-    
+    const mouseMove = (e) => {
+      const coors = { x: e.x, y: e.y };
+      setCoords( coors );
+    }
+    window.addEventListener('mousemove', mouseMove);
     return () => {
+      window.removeEventListener('mousemove', mouseMove);
       console.log('componente desmontado');
     }
   }, [])
@@ -12,6 +19,9 @@ const Message = () => {
   return (
     <div>
       <h3>Eres genial</h3>
+      <p>
+        x:{ x } y:{ y }
+      </p>
     </div>
   )
 }
