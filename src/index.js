692dba3001ac387c19d977dee5035101e349331b
stevenacz <scoailazaa@gmail.com>
2021-01-25 17:32:10 -0500

    useFetch - customHook

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import FormWithCustomHooks from './components/02-useEffect/FormWithCustomHook';
+// import FormWithCustomHooks from './components/02-useEffect/FormWithCustomHook';
 // import CounterApp from './components/01-useState/CounterApp';
 // import CounterWithCustomHook from './components/01-useState/CounterWithCustomHook';
 // import HookApp from './HookApp';
 // import SimpleForm from './components/02-useEffect/SimpleForm';
+import MultipleCustomHooks from './components/03-examples/MultipleCustomHooks';
 
 ReactDOM.render(
-  <FormWithCustomHooks />,
+  <MultipleCustomHooks />,
   document.getElementById('root')
 );
