9b909a21d93318931a52881ee22857af4f110a03
stevenacz <scoailazaa@gmail.com>
2021-02-01 18:40:23 -0500

    useCounter - customHook

--- src/components/01-useState/CounterWithCustomHook.js
+++ src/components/01-useState/CounterWithCustomHook.js
@@ -4,16 +4,16 @@ import './counter.css';
 
 const CounterWithCustomHook = () => {
 
-  const { state, increment, decrement, reset } = useCounter( 100 );
+  const { counter, increment, decrement, reset } = useCounter( 100 );
 
   return (
     <>
-      <h1>Counter with Hook: { state }</h1>
+      <h1>Counter with Hook: { counter }</h1>
       <hr />
 
-      <button onClick={ () => increment(2) } className="btn">+1</button>
+      <button onClick={ increment } className="btn">+1</button>
       <button onClick={ reset } className="btn">Reset</button>
-      <button onClick={ () => decrement(2) } className="btn">-1</button>
+      <button onClick={ decrement } className="btn">-1</button>
     </>
   )
 }
--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 // import FormWithCustomHooks from './components/02-useEffect/FormWithCustomHook';
-import CounterApp from './components/01-useState/CounterApp';
-// import CounterWithCustomHook from './components/01-useState/CounterWithCustomHook';
+// import CounterApp from './components/01-useState/CounterApp';
+import CounterWithCustomHook from './components/01-useState/CounterWithCustomHook';
 // import HookApp from './HookApp';
 // import SimpleForm from './components/02-useEffect/SimpleForm';
 // import MultipleCustomHooks from './components/03-examples/MultipleCustomHooks';
@@ -11,6 +11,7 @@ import CounterApp from './components/01-useState/CounterApp';
 
 ReactDOM.render(
   // <RealExampleRef />,
-  <CounterApp />,
+  // <CounterApp />,
+  <CounterWithCustomHook />,
   document.getElementById('root')
 );
