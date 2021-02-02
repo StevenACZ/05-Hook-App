7ae1894a8d89c86093706893f9967ca29de01402
stevenacz <scoailazaa@gmail.com>
2021-02-02 16:12:47 -0500

    useCallBack

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import MemoHook from './components/06-memos/MemoHook';
 // import Layout from './components/05-useLayoutEffect/Layout';
-import Memorize from './components/06-memos/Memorize';
+// import Memorize from './components/06-memos/Memorize';
 // import FormWithCustomHooks from './components/02-useEffect/FormWithCustomHook';
 // import CounterApp from './components/01-useState/CounterApp';
 // import CounterWithCustomHook from './components/01-useState/CounterWithCustomHook';
@@ -20,6 +21,7 @@ ReactDOM.render(
   // <MultipleCustomHooks />,
   // <FocusScreen />,
   // <Layout />,
-  <Memorize />,
+  // <Memorize />,
+  <MemoHook />,
   document.getElementById('root')
 );
