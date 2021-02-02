c90f0f22cbfee5a3ee9f6a185d54698472fb4d22
stevenacz <scoailazaa@gmail.com>
2021-02-02 14:52:08 -0500

    Caso de uso real - useRef

--- src/components/04-useRef/FocusScreen.js
+++ src/components/04-useRef/FocusScreen.js
@@ -3,26 +3,33 @@ import '../02-useEffect/effects.css'
 
 const FocusScreen = () => {
   const inputRef = useRef()
+  const inputRef1 = useRef()
   // console.log(ref)
 
   const handleClick = () => {
     inputRef.current.select();
-    console.log(inputRef)
+    console.log(inputRef1.current.placeholder)
+    console.log(inputRef.current.placeholder)
   }
 
   return (
     <div>
       <h1>Focus Screen</h1>
       <hr />
-
       <input
         ref={ inputRef }
         className="form-control"
         placeholder="Su nombre"
       />
 
+      <input
+        ref={ inputRef1 }
+        className="form-control"
+        placeholder="Su nombre"
+      />
+
       <button
-        className="btn btn-outline-primary mt-4"
+        className="btn btn-outline-primary mt-3"
         onClick={ handleClick }
       >
         Focus
--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import FormWithCustomHooks from './components/02-useEffect/FormWithCustomHook';
+// import FormWithCustomHooks from './components/02-useEffect/FormWithCustomHook';
 // import CounterApp from './components/01-useState/CounterApp';
 // import CounterWithCustomHook from './components/01-useState/CounterWithCustomHook';
 // import HookApp from './HookApp';
 // import SimpleForm from './components/02-useEffect/SimpleForm';
 // import MultipleCustomHooks from './components/03-examples/MultipleCustomHooks';
-// import FocusScreen from './components/04-useRef/FocusScreen';
+import FocusScreen from './components/04-useRef/FocusScreen';
 // import RealExampleRef from './components/04-useRef/RealExampleRef';
 
 ReactDOM.render(
@@ -14,6 +14,8 @@ ReactDOM.render(
   // <CounterApp />,
   // <CounterWithCustomHook />,
   // <SimpleForm />,
-  <FormWithCustomHooks />,
+  // <FormWithCustomHooks />,
+  // <MultipleCustomHooks />,
+  <FocusScreen />,
   document.getElementById('root')
 );
