aaee9322969dccdb65988d031fa65932e0c96bb9
stevenacz <scoailazaa@gmail.com>
2021-02-02 21:08:31 -0500

    Idea general de un reducer

--- src/index.js
+++ src/index.js
@@ -1,31 +1,33 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-// import CallbackHook from './components/06-memos/CallbackHook';
-import { Padre } from './components/07-tarea-memo/Padre';
-// import MemoHook from './components/06-memos/MemoHook';
-// import Layout from './components/05-useLayoutEffect/Layout';
-// import Memorize from './components/06-memos/Memorize';
-// import FormWithCustomHooks from './components/02-useEffect/FormWithCustomHook';
-// import CounterApp from './components/01-useState/CounterApp';
-// import CounterWithCustomHook from './components/01-useState/CounterWithCustomHook';
-// import HookApp from './HookApp';
-// import SimpleForm from './components/02-useEffect/SimpleForm';
-// import MultipleCustomHooks from './components/03-examples/MultipleCustomHooks';
-// import FocusScreen from './components/04-useRef/FocusScreen';
-// import RealExampleRef from './components/04-useRef/RealExampleRef';
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// // import CallbackHook from './components/06-memos/CallbackHook';
+// import { Padre } from './components/07-tarea-memo/Padre';
+// // import MemoHook from './components/06-memos/MemoHook';
+// // import Layout from './components/05-useLayoutEffect/Layout';
+// // import Memorize from './components/06-memos/Memorize';
+// // import FormWithCustomHooks from './components/02-useEffect/FormWithCustomHook';
+// // import CounterApp from './components/01-useState/CounterApp';
+// // import CounterWithCustomHook from './components/01-useState/CounterWithCustomHook';
+// // import HookApp from './HookApp';
+// // import SimpleForm from './components/02-useEffect/SimpleForm';
+// // import MultipleCustomHooks from './components/03-examples/MultipleCustomHooks';
+// // import FocusScreen from './components/04-useRef/FocusScreen';
+// // import RealExampleRef from './components/04-useRef/RealExampleRef';
 
-ReactDOM.render(
-  // <RealExampleRef />,
-  // <CounterApp />,
-  // <CounterWithCustomHook />,
-  // <SimpleForm />,
-  // <FormWithCustomHooks />,
-  // <MultipleCustomHooks />,
-  // <FocusScreen />,
-  // <Layout />,
-  // <Memorize />,
-  // <MemoHook />,
-  // <CallbackHook />,
-  <Padre/>,
-  document.getElementById('root')
-);
+// ReactDOM.render(
+//   // <RealExampleRef />,
+//   // <CounterApp />,
+//   // <CounterWithCustomHook />,
+//   // <SimpleForm />,
+//   // <FormWithCustomHooks />,
+//   // <MultipleCustomHooks />,
+//   // <FocusScreen />,
+//   // <Layout />,
+//   // <Memorize />,
+//   // <MemoHook />,
+//   // <CallbackHook />,
+//   <Padre/>,
+//   document.getElementById('root')
+// );
+
+import './components/08-useReducer/intro-reducer';
